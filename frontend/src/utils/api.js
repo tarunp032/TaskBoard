563cb58ee23b8f90b4b2fad6cbec68366486c497
tarunp032 <prajapattarun7568@gmail.com>
2025-11-15 13:15:02 +0530

    Update API baseURL for Render

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://taskboard-backend-tech.onrender.com/api'
 });
 
 // Add token to every request
